3f3c3c570368ef392baaaa4674785444f58861d4
Kennedy <kennedycontato46@gmail.com>
2020-07-15 17:14:19 -0300

    Realizados testes de diferenciacao de formatacoes e adicionadas imagens

--- js/script.js
+++ js/script.js
@@ -1,7 +1,46 @@
+let slide_ativo = '0';
 function openNav() {
   document.getElementById("mySidenav").style.width = "300px";
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
+}
+function menuSelect(tgt) {
+	slide_ativo = tgt.substring(3, 6);
+	let option = document.getElementById(tgt);
+	option.selected = 1;
+	let a = document.getElementById('conteudo');
+	let b = document.getElementById('conteudo'+slide_ativo);
+	a.innerHTML = b.innerHTML;
+}
+function avancaSlide() {
+	let parse = parseInt(slide_ativo, 10)+1;
+	if (parse<32) {
+		let opt = document.getElementById('opt'+parse);
+		opt.selected = 1;
+		slide_ativo = parse;
+	}
+	let a = document.getElementById('conteudo');
+	let b = document.getElementById('div'+slide_ativo);
+	a.innerHTML = b.innerHTML;
+}
+function voltaSlide() {
+	let parse = parseInt(slide_ativo, 10)-1;
+	if (parse>-1) {
+		let opt = document.getElementById('opt'+parse);
+		opt.selected = 1;
+		slide_ativo = parse;
+		let a = document.getElementById('conteudo');
+		let b = document.getElementById('div'+slide_ativo);
+		a.innerHTML = b.innerHTML;
+	}
+}
+function voltarAoInicio(tgt){
+	slide_ativo = tgt.substring(3, 6);
+	let option = document.getElementById(tgt);
+	option.selected = 1;
+	let a = document.getElementById('conteudo');
+	let b = document.getElementById('conteudo0');
+	a.innerHTML = b.innerHTML;
 }
